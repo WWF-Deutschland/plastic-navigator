ad7db32f83af1ce55b4d1fac7bf4b04bfadca386
Timo Franz <timo@dumpark.com>
2020-07-04 18:16:50 +0200

    fix layer reference intl

--- app/containers/LayerInfo/LayerReference.js
+++ app/containers/LayerInfo/LayerReference.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 import styled from 'styled-components';
+
+import { DEFAULT_LOCALE } from 'i18n';
 
 import messages from './messages';
 
@@ -23,20 +25,23 @@ const Publication = styled.span`
 `;
 const URL = styled.a``;
 
-export function LayerReference({ attribution }) {
+export function LayerReference({ attribution, intl }) {
   const { author, year, title, publication, url } = attribution;
+  const locale = intl.locale || DEFAULT_LOCALE;
   return (
     <Styled>
       <Title>
         <FormattedMessage {...messages.titleReference} />
       </Title>
       <Reference>
-        {author && <Author>{author}</Author>}
+        {author && <Author>{author[locale] || author}</Author>}
         {year && <Year> ({year})</Year>}
         {'. '}
-        {title && <Study>{title}</Study>}
+        {title && <Study>{title[locale] || title}</Study>}
         {title && <span>. </span>}
-        {publication && <Publication>{publication}</Publication>}
+        {publication && (
+          <Publication>{publication[locale] || publication}</Publication>
+        )}
         {publication && <span>. </span>}
         {url && (
           <URL target="_blank" href={url}>
@@ -50,6 +55,7 @@ export function LayerReference({ attribution }) {
 
 LayerReference.propTypes = {
   attribution: PropTypes.object,
+  intl: intlShape.isRequired,
 };
 
-export default LayerReference;
+export default injectIntl(LayerReference);
