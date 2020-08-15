3b8524342405c7e003bdf0ae05a5f3983979c82d
Timo Franz <timo@dumpark.com>
2020-08-15 13:21:52 +0200

    update tooltip

--- app/containers/Map/Tooltip.js
+++ app/containers/Map/Tooltip.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { injectIntl, intlShape } from 'react-intl';
-import { Box, Button } from 'grommet';
+import { Button, Text } from 'grommet';
 import { Close } from 'grommet-icons';
 
 import { roundNumber } from 'utils/numbers';
 
 import TooltipContent from './TooltipContent';
+import messages from './messages';
 
 import { getPropertyByLocale } from './utils';
 
@@ -34,6 +35,7 @@ const Anchor = styled.div`
   top: ${({ xy }) => xy.y}px;
   left: ${({ dirLeft, xy, w }) =>
     dirLeft ? -1 * (xy.x + w) : xy.x}px;
+  background: white;
   &::after {
     content: '';
     width: 0;
@@ -49,46 +51,50 @@ const Anchor = styled.div`
     left: ${({ dirLeft, w }) => (dirLeft ? w - 10 : 'auto')}${({ dirLeft }) => (dirLeft ? 'px' : '')};
   }
 `;
+
+// Had to go with inline-styles as the styled-components weirdly caused the WWF
+// font to flickr/relaod when opened for the first time
+
 // eslint-ebable prefer-template
 // border-right-color: ${({ dirLeft }) => (!dirLeft ? 'white' : 'transparent')};
 
-const Main = styled.div`
-  position: absolute;
-  top: -40px;
-  left: ${({ dirLeft }) => (dirLeft ? -10 : 10)}px;
-  display: block;
-  background: white;
-  box-shadow: 0px 0px 12px 0px rgba(0, 0, 0, 0.2);
-  width: ${({ w }) => w}px;
-  min-height: 100px;
-  pointer-events: all;
-`;
+// const Main = styled.div`
+//   position: absolute;
+//   top: -40px;
+//   left: ${({ dirLeft }) => (dirLeft ? -10 : 10)}px;
+//   display: block;
+//   background: white;
+//   box-shadow: 0px 0px 12px 0px rgba(0, 0, 0, 0.2);
+//   width: ${({ w }) => w}px;
+//   min-height: 100px;
+//   pointer-events: all;
+// `;
+//
+// const Title = styled.div`
+//   font-family: 'wwfregular';
+//   text-transform: uppercase;
+//   font-weight: normal;
+//   letter-spacing: 0.05em;
+//   font-size: 1.4em;
+//   line-height: 1.1em;
+//   margin-bottom: 20px;
+// `;
+// const SupTitle = styled.div`
+//   font-weight: bold;
+//   text-transform: uppercase;
+//   font-size: 0.8em;
+// `;
 
-const Title = styled.div`
-  font-family: 'wwfregular';
-  text-transform: uppercase;
-  font-weight: normal;
-  letter-spacing: 0.05em;
-  font-size: 1.4em;
-  line-height: 1.1em;
-  margin-bottom: 20px;
-`;
-const SupTitle = styled.div`
-  font-weight: bold;
-  text-transform: uppercase;
-  font-size: 0.8em;
-`;
-
-const CloseWrap = styled.div`
-  position: absolute;
-  right: -10px;
-  top: -10px;
-  width: 30px;
-  height: 30px;
-  border-radius: 9999px;
-  background: white;
-  box-shadow: 0px 0px 12px 0px rgba(0, 0, 0, 0.2);
-`;
+// const CloseWrap = styled.div`
+//   position: absolute;
+//   right: -10px;
+//   top: -10px;
+//   width: 30px;
+//   height: 30px;
+//   border-radius: 9999px;
+//   background: white;
+//   box-shadow: 0px 0px 12px 0px rgba(0, 0, 0, 0.2);
+// `;
 
 const getTitle = (feature, config, layer, intl) => {
   const { locale } = intl;
@@ -175,30 +181,78 @@ const Tooltip = ({
   const markerSize = getMarkerSize(config, feature, layerOptions);
   const offset = getOffset(config, feature, markerSize);
   const layer = layerOptions ? layerOptions.layer : null;
-
+  const supTitle =
+    tooltip.supTitle && getSupTitle(feature, config, layer, locale);
+  const title = tooltip.title && getTitle(feature, config, layer, intl);
   // prettier-ignore
   return (
     <Root position={position}>
       <Anchor dirLeft={direction.x === 'left'} xy={offset} w={WIDTH}>
-        <Main
-          dirLeft={direction.x === 'left'}
-          w={WIDTH}
+        <div
+          style={{
+            position: 'absolute',
+            top: '-40px',
+            left: direction.x === 'left' ? '-10px' : '10px',
+            display: 'block',
+            background: 'white',
+            boxShadow: '0px 0px 12px 0px rgba(0, 0, 0, 0.2)',
+            width: `${WIDTH}px`,
+            minHeight: '100px',
+            pointerEvents: 'all',
+            padding: '10px',
+          }}
         >
-          <Box margin="small">
-            {tooltip.supTitle && (
-              <SupTitle>{getSupTitle(feature, config, layer, locale)}</SupTitle>
-            )}
-            {tooltip.title && (
-              <Title>{getTitle(feature, config, layer, intl)}</Title>
-            )}
-            {tooltip.content && (
-              <TooltipContent feature={feature} config={config} layer={layer} />
-            )}
-            {(tooltip.more || tooltip.more === 'true') && tooltip.more !== 'false' && (
+          {supTitle && (
+            <div
+              style={{
+                fontWeight: 'bold',
+                textTransform: 'uppercase',
+                fontSize: '0.8em',
+              }}
+            >
+              {supTitle}
+            </div>
+          )}
+          {title && (
+            <div
+              style={{
+                fontFamily: 'wwfregular',
+                textTransform: 'uppercase',
+                fontWeight: 'normal',
+                letterSpacing: '0.05em',
+                fontSize: '1.4em',
+                lineHeight: '1.1em',
+                marginBottom: '20px',
+              }}
+            >
+              {title}
+            </div>
+          )}
+          {tooltip.content && (
+            <TooltipContent feature={feature} config={config} layer={layer} />
+          )}
+          {(tooltip.more || tooltip.more === 'true') && tooltip.more !== 'false' && (
+            <div
+              style={{
+                position: 'absolute',
+                bottom: '0',
+                right: '10px',
+                transform: 'translateY(50%)',
+              }}
+            >
               <Button
-                label="About"
+                label={
+                  <Text size="small">
+                    {intl.formatMessage(messages.tooltipMore)}
+                  </Text>
+                }
                 plain
-                style={{ textDecoration: 'underline' }}
+                style={{
+                  borderRadius: '20px',
+                  background: '#00728F',
+                  color: 'white',
+                  padding: '3px 10px',
+                }}
                 onClick={() => {
                   onFeatureClick({
                     feature: feature.properties.f_id,
@@ -209,9 +263,20 @@ const Tooltip = ({
                   });
                 }}
               />
-            )}
-          </Box>
-          <CloseWrap>
+            </div>
+          )}
+          <div
+            style={{
+              position: 'absolute',
+              right: '-10px',
+              top: '-10px',
+              width: '30px',
+              height: '30px',
+              borderRadius: '9999px',
+              background: 'white',
+              boxShadow: '0px 0px 12px 0px rgba(0, 0, 0, 0.2)',
+            }}
+          >
             <Button
               plain
               onClick={() => onClose()}
@@ -221,8 +286,8 @@ const Tooltip = ({
                 textAlign: 'center',
               }}
             />
-          </CloseWrap>
-        </Main>
+          </div>
+        </div>
       </Anchor>
     </Root>
   );
---
+++ app/containers/Map/messages.js
@@ -0,0 +1,16 @@
+/*
+ * PanelExplore Messages
+ *
+ * This contains all the text for the PanelExplore container.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export const scope = 'app.containers.Map';
+
+export default defineMessages({
+  tooltipMore: {
+    id: `${scope}.tooltipMore`,
+    defaultMessage: 'Read more',
+  },
+});
