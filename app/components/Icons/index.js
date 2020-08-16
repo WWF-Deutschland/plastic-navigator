bf78acf904ed49dfc9f1092f3c4ef4f6e52e3677
Timo Franz <timo@dumpark.com>
2020-08-16 13:02:40 +0200

    more Icons

--- app/components/Icons/index.js
+++ app/components/Icons/index.js
@@ -1,6 +1,8 @@
 import Explore from './Explore';
+import ArrowDown from './ArrowDown';
 import ArrowLeft from './ArrowLeft';
 import ArrowRight from './ArrowRight';
+import ArrowUp from './ArrowUp';
 import Close from './Close';
 import DropdownClose from './DropdownClose';
 import DropdownOpen from './DropdownOpen';
@@ -8,6 +10,8 @@ import ExploreSmall from './ExploreSmall';
 import InfoOutline from './InfoOutline';
 import InfoSolid from './InfoSolid';
 import Intro from './Intro';
+import Menu from './Menu';
+import MenuOpen from './MenuOpen';
 import Minus from './Minus';
 import Plus from './Plus';
 import WWFLogoHeader from './WWFLogoHeader';
@@ -15,8 +19,10 @@ import WWFLogoSmall from './WWFLogoSmall';
 
 export {
   Explore,
+  ArrowDown,
   ArrowLeft,
   ArrowRight,
+  ArrowUp,
   Close,
   DropdownClose,
   DropdownOpen,
@@ -24,6 +30,8 @@ export {
   InfoOutline,
   InfoSolid,
   Intro,
+  Menu,
+  MenuOpen,
   Minus,
   Plus,
   WWFLogoHeader,
