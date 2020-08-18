ec578e38d534eeb4c4f0dea27e8c74b022a1a314
edith <edith@dumpark.com>
2020-08-18 08:14:12 +0200

    update intro icon

--- app/components/Icons/Intro.js
+++ app/components/Icons/Intro.js
@@ -7,7 +7,7 @@ function Intro(props) {
         <path
           fill="inherit"
           stroke="inherit"
-          d="M28,9.59V3H6V31h4v4H32V13.59ZM10,29H8V5H26V7.59L25.41,7H10Zm20,4H12V9H24v6h6ZM15,21.5H27v1H15Zm0,3H27v1H15Zm0,3h6v1H15Z"
+          d="M28,9.59V3H6V31h4v4H32V13.59ZM10,29H8V5H26V7.59L25.41,7H10Zm20,4H12V9H24v6h6ZM15,21H27v1H15Zm0,3H27v1H15Zm0,3h6v1H15Z"
         />
       </g>
     </StyledIcon>
