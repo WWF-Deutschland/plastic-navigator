5cdb5f2ac02c69615b23bcf4ea0606c27062c822
tmfrnz <timo@dumpark.com>
2023-04-13 11:34:51 +0200

    show topic annotation for

--- app/containers/PanelKey/utils.js
+++ app/containers/PanelKey/utils.js
@@ -13,35 +13,34 @@ export const getLayerTitle = ({ intl, config, jsonLayerInfo, indicatorId }) => {
   let title = 'no title';
   if (config && config.title) {
     title = config.title[locale] || config.title[DEFAULT_LOCALE];
-  }
-  // append
-  if (config.id === POLICY_LAYER && jsonLayerInfo && indicatorId) {
-    title = `${title}: ${getTopicTitle({
-      layerInfo: jsonLayerInfo,
-      indicatorId,
-      locale,
-    })}`;
-  }
-  if (config.id === PROJECT_CONFIG.id) {
+    // append
+    if (config.id === POLICY_LAYER && jsonLayerInfo && indicatorId) {
+      title = `${title}: ${getTopicTitle({
+        layerInfo: jsonLayerInfo,
+        indicatorId,
+        locale,
+      })}`;
+    }
+  } else if (config.id === PROJECT_CONFIG.id) {
     title = intl.formatMessage(messages.keyProjectsTitle);
   }
-  return title;
+  return !title || title.trim() === '' ? 'title' : title;
 };
 
 export const getLayerAbout = ({ intl, config, jsonLayerInfo, indicatorId }) => {
   const { locale } = intl;
-  if (config && config.about) {
-    return config.about[locale] || config.about[DEFAULT_LOCALE];
-  }
+  let annotation;
   if (jsonLayerInfo && config.id === POLICY_LAYER) {
-    return getTopicMapAnnotation({
+    annotation = getTopicMapAnnotation({
       layerInfo: jsonLayerInfo,
       indicatorId,
       locale,
     });
+  } else if (config.id === PROJECT_CONFIG.id) {
+    annotation = intl.formatMessage(messages.keyProjectsAbout);
   }
-  if (config.id === PROJECT_CONFIG.id) {
-    return intl.formatMessage(messages.keyProjectsAbout);
+  if ((!annotation || annotation.trim() === '') && config && config.about) {
+    annotation = config.about[locale] || config.about[DEFAULT_LOCALE];
   }
-  return 'about';
+  return !annotation || annotation.trim() === '' ? 'about' : annotation;
 };
